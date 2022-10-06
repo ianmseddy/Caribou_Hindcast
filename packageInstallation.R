8c3ab15a8ad121a8db5e65be05ac607518bbcfd9
Ian Eddy <ianmseddy@gmail.com>
2022-10-06 17:39:09 +0000

    bad url for canopy cover 1985

--- packageInstallation.R
+++ packageInstallation.R
@@ -7,7 +7,7 @@ if (!dir.exists(packageDir)) dir.create(packageDir)
 if (!require("Require")) install.packages("Require")
 Require("checkpoint")
 Require("remotes")
-checkpoint("2021-01-01", r_version = "4.2.0", checkpoint_location = packageDir)
+# checkpoint("2021-01-01", r_version = "4.2.0", checkpoint_location = packageDir)
 
 Require(c("terra", "sf", "data.table", "PredictiveEcology/reproducible (>= 1.2.10.9001)",
           "stringr", "raster", "googledrive", "SpaDES.tools"))
