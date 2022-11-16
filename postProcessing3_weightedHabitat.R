a531cbd560882f978fc70b26c70034a118d599f0
Ian Eddy <ianmseddy@gmail.com>
2022-11-16 09:36:07 -0800

    correct typo

--- postProcessing3_weightedHabitat.R
+++ postProcessing3_weightedHabitat.R
@@ -2,15 +2,18 @@
 # convert all focal habitat rasters into single weighted habitat layer for each year
 # calculate the difference
 # generate a habitat class layer by assigning 1:8 to binary habitats
-outputDir <- "D:/Ian/YanBoulanger/focalHabitat"
+inputDir <- "D:/Ian/YanBoulanger/focalHabitat"
+outputDir <- "D:/Ian/YanBoulanger/weightedHabitat"
+checkPath(outputDir, create = TRUE)
 tiles <- paste0("tile", 1:6)
 
-habitatRasters <- lapply(tiles, list.files, path = outputDir, full.names = TRUE)
+habitatRasters <- lapply(tiles, list.files, path = inputDir, full.names = TRUE)
 
 makeWeightedHabitat <- function(tileList, year, outputPath) {
 
 
   tileList <- tileList[grep(tileList, pattern = year)] #1985 or 2020
+  if (length(tileList) == 0) {stop("incorrect year")}
   #as a float, each individual habitat tile is 20 GB
 
   fire <- rast(tileList[grep("naturalDisturbance", tileList)]) * 0.06
@@ -27,11 +30,10 @@ makeWeightedHabitat <- function(tileList, year, outputPath) {
   gc()
 
   woodlands <- rast(tileList[grep("openWoodland", tileList)]) * 0.22
-  wetlands <- rast(tileList[grep("wetland", tileList)]) * 0.06
+  wetlands <- rast(tileList[grep("wetland", tileList)]) * 0.14
   weightedHabitat <- sum(weightedHabitat, woodlands, wetlands, na.rm = TRUE)
   rm(woodlands, wetlands)
   gc()
-
 
   regen <- rast(tileList[grep("regenerating", tileList)]) * 0.06
   weightedHabitat <- sum(weightedHabitat, regen, na.rm = TRUE)
