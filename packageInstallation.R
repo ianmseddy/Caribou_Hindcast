90467eacc849704482993030d3224298eaff541d
ianmseddy <ianmseddy@gmail.com>
2024-05-14 15:48:41 -0700

    packages should be stored outside Rprojects
    Prevent some weird behavior of Rstudio

--- packageInstallation.R
+++ packageInstallation.R
@@ -1,37 +1,38 @@
 #project set up
-pkgDir <- file.path("packages",version$platform, getRversion()[, 1:2])
+
+#first make a custom package library
+pkgDir <- file.path(tools::R_user_dir("data"), "Caribou_Hindcast", "packages",
+                    version$platform, substr(getRversion(), 1, 3))
+#run this once
 if (FALSE) {
   dir.create(pkgDir, recursive = TRUE, showWarnings = FALSE)
   .libPaths(pkgDir, include.site = FALSE)
   message("Using libPaths:\n", paste(.libPaths(), collapse = "\n"))
 
-  #while require is down
-  # if (!require("Require")) install.packages("Require")
-  unloadNamespace("reproducible")
+  install.packages("Require")
   library(Require)
-
-  # Require("checkpoint", upgrade = FALSE) #checkpoint is deprecated in a few months
   Require("remotes", upgrade = FALSE)
-  # checkpoint("2021-01-01", r_version = "4.2.0", checkpoint_location = packageDir)
 
+  Require::setLibPaths()
   #at the moment this terra version throws obnoxious but harmless errors. Upgrading to source would solve the issue,
   #but the source version was unavailable when I began this project.
-  # Require::Require(packages = c("terra (>=1.6.7)", "sf", "data.table", "reproducible (>=1.2.10.9001)",
-  #           "stringr", "raster", "googledrive", "magrittr", "parallel", "SpaDES.tools", "fasterize"),
-  #         upgrade = FALSE)
+  Require::Require(packages = c("terra (>=1.6.7)", "sf", "data.table", "reproducible (>=1.2.10.9001)",
+            "stringr", "raster", "googledrive", "magrittr", "parallel", "SpaDES.tools", "fasterize"),
+          upgrade = FALSE)
 }
 
+.libPaths(pkgDir, include.site = FALSE)
 library("terra")
 library("sf")
 library("data.table")
 library("reproducible")
 library("stringr")
-library("raster")
-library("googledrive")
-library("magrittr")
-library("parallel")
-library("SpaDES.tools")
-library("fasterize")
+library("raster") #
+library("googledrive") #uploading results
+library("magrittr") #for piping, before |> came along
+library("parallel") #for focal operations
+library("SpaDES.tools") #for splitRaster into tiles
+library("fasterize") #turning vectors into rasters
 
 # packageVersion('terra') #1.6.7
 # packageVersion('reproducible') 1.2.10.9001
@@ -39,3 +40,5 @@ library("fasterize")
 
 #this is essential to ensuring GIS operations are identical across machines, so the tiles are the same
 #updating terra resulted in a one-row difference in the projected raster, which meant everything had to be re-cropped
+#so while the version does not have to be 1.6.7, it should be the same if run on multiple machines
+
